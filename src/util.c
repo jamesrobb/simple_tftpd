f9c85b6139fb804287864fcc02849900bd3d1ad0
James Robb <me@jamesrobb.ca>
2016-09-09 16:00:08 +0000

    removing extraneous variables

--- src/util.c
+++ src/util.c
@@ -1,9 +1,6 @@
 #include "util.h"
 
 int find_string_terminator(char* buffer, int buffer_len, int start) {
-	int end_index = 0;
-
-    //printf("buffer siize: %ld\n", sizeof(buffer));
 
 	for(int i = start; i < buffer_len; i++) {
 
